6689ce56cf8f830dee42cfe9ca5337b374ebc5a6
Vince Scott <vince@openanswers.co.uk>
2019-10-04 17:36:17 +0100

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,7 @@ import { inspect } from "util";
 import { EngineManager } from "./pico";
 import { Context } from "./pico/context";
 import { FsProvider } from "./providers";
-import { interval, timer } from "rxjs";
-import { map, switchMap } from "rxjs/operators";
-
-const ruleDoc = {
-  label: "some rule",
-  if: [
-    { op: "like", token: "summary", value: "(?<first>[a-z]+) (?<second>[a-z]+)" },
-    { op: "eq", token: "node", value: "localhost", lop: 1 },
-    {
-      op: "list",
-      conditions: [{ op: "list", traversal: "and", conditions: [{ op: "eq", token: "group", value: "production" }] }],
-    },
-  ],
-  then: [
-    { act: "setvar", varName: "pop", varValue: "john" },
-    { act: "template", varName: "popppy", template: "from node {{node}}" },
-    {
-      act: "rule",
-      rule: { label: "subrule", if: [{ op: "eq", token: "customer", value: "GM" }], then: [], else: [] },
-    },
-  ],
-  else: [],
-};
-
-const picoRules = { main: [ruleDoc] };
+import { interval } from "rxjs";
 
 const fsp = new FsProvider({ filepath: "rules.json" });
 
@@ -73,31 +49,3 @@ source.subscribe(t => {
   }
   console.log("end " + new Date().getTime());
 });
-
-//em.load(picoRules).then(engine => {
-/*
-em.loadFromFile("rules.json").subscribe(
-  engine => {
-    let context = new Context();
-    context.tokens.set("node", "localhost");
-    context.tokens.set("summary", "hello world");
-
-    engine.exec(context);
-    console.log("CTX tokens: ", Object.fromEntries(context.tokens));
-    console.log("CTX locals: ", Object.fromEntries(context.locals));
-  },
-  err => {
-    console.log("failure", err);
-  },
-  () => {
-    console.log("COMPLETED");
-  }
-);
-*/
-/*
-const fp = new FsProvider("rules.json");
-
-fp.emit().subscribe(f => {
-  console.log("loaded file", f);
-});
-*/
