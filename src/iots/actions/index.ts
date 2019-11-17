c8d2f58272c5cffdc2f5b5fd6a266d4138d71d75
Vince Scott <vince@openanswers.co.uk>
2019-11-17 16:12:43 +0000

    recursion

--- src/iots/actions/index.ts
+++ src/iots/actions/index.ts
@@ -4,7 +4,10 @@ import { PicoActionRule } from "../rules";
 
 export { PicoActionSetVar, PicoActionRule };
 
-export const PicoAction = t.union([PicoActionRule, PicoActionSetVar]);
+//export const PicoAction = t.union([PicoActionRule, PicoActionSetVar]);
+export const PicoAction: t.Type<PicoActionRule | PicoActionSetVar> = t.recursion("PicoAction", () =>
+  t.union([PicoActionRule, PicoActionSetVar])
+);
 export type PicoAction = t.TypeOf<typeof PicoAction>;
 
 export const PicoActionCollection = t.array(PicoAction);
--- src/iots/actions/setvar.ts
+++ src/iots/actions/setvar.ts
@@ -3,5 +3,7 @@ import * as t from "io-ts";
 export const PicoActionSetVar = t.type({
   act: t.literal("setvar"),
   varName: t.string,
-  varValue: t.string
+  varValue: t.string,
 });
+
+export type PicoActionSetVar = t.TypeOf<typeof PicoActionSetVar>;
